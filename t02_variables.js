0c9e02a9ce19e176547b2ec59537516fb183cf6b
23033ln <23033ln@hvhs.school.nz>
2025-04-29 23:51:50 +0000

    Apr 29, 2025, 11:51 PM

--- t02_variables.js
+++ t02_variables.js
@@ -1,5 +1,27 @@
+/****************************
+ Intronductions
+ Lukas
+29/04/25 
+Intro
+*****************************/
+console.log("Running t01_introduction.js");
+console.log("Josh likes touching planes");
+
+//variables
+
 var score = 100;
-score = "100";
-score = "110";
-Write("score");
-console.log("score")
+score = 100;
+score = 110;
+Write(score);
+console.log(score);
+
+/**************************** 
+ *Main code
+ ****************************/ 
+
+
+
+
+/****************************
+ funtions
+ ****************************/
