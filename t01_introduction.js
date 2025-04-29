e195ccb2d5024474fb43dcc6d31c5b66806717a4
23033ln <23033ln@hvhs.school.nz>
2025-04-29 23:49:08 +0000

    Apr 29, 2025, 11:49 PM

--- t01_introduction.js
+++ t01_introduction.js
@@ -6,23 +6,13 @@ Intro
 *****************************/
 console.log("Running t01_introduction.js");
 console.log("Josh likes touching planes");
-console.log("Josh likes touching planes");
-console.log("Josh likes touching planes");
-console.log("Josh likes touching planes");
-console.log("Josh likes touching planes");
-console.log("Josh likes touching planes");
-console.log("Josh likes touching planes");
-console.log("Josh likes touching planes");
-console.log("Josh likes touching planes");
-console.log("Josh likes touching planes");
-console.log("Josh likes touching planes");
-console.log("Josh likes touching planes");
-console.log("Josh likes touching planes");
-console.log("Josh likes touching planes");
 
 //variables
-
-
+var score = 100;
+score = "100";
+score = "110";
+Write("score");
+console.log("score")
 /**************************** 
  *Main code
  ****************************/ 
