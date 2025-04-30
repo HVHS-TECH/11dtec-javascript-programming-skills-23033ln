487fe62cafd72e005b5a160a9179516131dbf42f
23033ln <23033ln@hvhs.school.nz>
2025-04-30 00:20:47 +0000

    Apr 30, 2025, 12:20 AM

--- t03_maths.js
+++ t03_maths.js
@@ -8,6 +8,14 @@ Intro
 
 //variables
 
+let num1 = 80;
+let num2 = 10;
+console.log("Add", num1 + num2);
+console.log("subtract", num1 - num2);
+console.log("mutilpy", num1 * num2);
+console.log("divide", num1 / num2);
+
+
 
 
 /**************************** 
