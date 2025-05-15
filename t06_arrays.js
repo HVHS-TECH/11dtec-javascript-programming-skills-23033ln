27c1967dd6cc9c2ef385bf2df81f96527c87ab8b
23033ln <23033ln@hvhs.school.nz>
2025-05-15 00:34:11 +0000

    May 15, 2025, 12:34 AM

--- t06_arrays.js
+++ t06_arrays.js
@@ -1,2 +1,22 @@
+
+/****************************
+ Intronductions
+ Lukas
+29/04/25 
+Intro
+*****************************/
+
+
+//variables
 let classarray = ["jack, josh, coby, cam"];
 alert("Arrayays 4th item: " + classarray[3]);
+/**************************** 
+ *Main code
+ ****************************/ 
+
+
+
+
+/****************************
+ funtions
+ ****************************/
---
+++ t12_while_loop.js
@@ -0,0 +1,22 @@
+/****************************
+ Intronductions
+ Lukas
+29/04/25 
+Intro
+*****************************/
+//variables
+/**************************** 
+ *Main code
+ ****************************/ 
+
+var password = prompt("speak Friend and enter");
+
+while (password != "hihihi"){
+    password = prompt("Thats not the password. /n try again")
+}
+
+alert("Welcome to the website");
+
+/****************************
+ funtions
+ ****************************/
