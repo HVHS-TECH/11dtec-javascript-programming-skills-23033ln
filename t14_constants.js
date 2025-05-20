6386cd92ccb41c7bb8a65d8f0c9d2ef75a3fd4db
23033ln <23033ln@hvhs.school.nz>
2025-05-20 23:30:46 +0000

    May 20, 2025, 11:30 PM

--- t14_constants.js
+++ t14_constants.js
@@ -5,10 +5,18 @@
 Intro
 *****************************/
 //variables
-
+const DRIVINGAGE;
+var  userAge;
 /**************************** 
  *Main code
  ****************************/
+
+ userAge = prompt("whats your age");
+ if( userAge >= DRIVINGAGE) {
+        alert("you are enough to drive");
+    }else{
+        alert("you are underage you can't drive");
+    }
 
 /****************************
  funtions
